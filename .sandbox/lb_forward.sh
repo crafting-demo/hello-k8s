f584a5a0821857fc423e7456ee9d8fc383a3ebdf
Yisui Hu <hu@crafting.dev>
2022-11-23 20:48:49 +0000

    detect load balancer ip or hostname

--- .sandbox/lb_forward.sh
+++ .sandbox/lb_forward.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 while true; do
-    IP=$(kubectl -n $APP_NS get svc frontend-lb -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-    if [[ -z "$IP" ]]; then
-        echo "External load balancer IP unavailable ..."
+    HOST=$(kubectl -n $APP_NS get svc frontend-lb -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+    [[ -n "$HOST" ]] || HOST=$(kubectl -n $APP_NS get svc frontend-lb -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
+    if [[ -z "$HOST" ]]; then
+        echo "External load balancer IP/Hostname unavailable ..."
         sleep 1
         continue
     fi
-    echo "External load balancer IP: $IP"
-    socat TCP-LISTEN:3800,fork,reuseaddr TCP-CONNECT:$IP:80
+    echo "External load balancer: $HOST"
+    socat TCP-LISTEN:3800,fork,reuseaddr TCP-CONNECT:$HOST:80
 done
